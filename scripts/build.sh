dd00c25068fad6b3e960b03c6f695bd8e23fa481
Benjamin Buch <benni.buch@gmail.com>
2018-11-01 12:57:07 +0100

    prepare for GitLab-CI

---
+++ scripts/build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# exit script on any error
+set -e
+set -o xtrace
+
+# set env variables
+COMPILER=$1
+COMPILER_VERSION=$($COMPILER --version | head -n1 | grep -Po '\d.\d.\d' | head -1)
+MODE=$2
+COMPILER_DIR=$COMPILER-$COMPILER_VERSION
+
+
+PROJECT_DIR=$(pwd)
+BIN_DIR=$PROJECT_DIR/bin/$COMPILER_DIR/$MODE
+BUILD_DIR=$BIN_DIR/build
+
+EXE_DIR=$BIN_DIR/AppDir/bin
+USR_BIN_DIR=$BIN_DIR/AppDir/usr/bin
+USR_LIB_DIR=$BIN_DIR/AppDir/usr/lib
+QT_PLUGINS_DIR=$BIN_DIR/AppDir/usr/qt/plugins
+
+
+## == build ==
+cd $BUILD_DIR
+qmake "CONFIG+=$MODE" ../../..
+make
+
+
+# == prepare AppImage ==
+
+# make build dirs if they don't exist already
+mkdir -p $BUILD_DIR
+mkdir -p $EXE_DIR
+mkdir -p $USR_BIN_DIR
+mkdir -p $USR_LIB_DIR
+mkdir -p $QT_PLUGINS_DIR
+
+# add main executable
+cp $MAIN_EXE $EXE_DIR/
+
+# add secondary executable
+cp /bin/grep $USR_BIN_DIR
+
+# add dependencies
+MAIN_EXE=$BUILD_DIR/BitmapViewer
+ldd $MAIN_EXE
+ldd $MAIN_EXE | grep -e "\.so" | grep -o '=> [^ ]*' | grep -o '[^=> ]*' | sort | sed -e 's/^/cp /' -e 's@$@ '"$USR_LIB_DIR/"'@' | bash
+
+# add QT plugins
+cp -r $Qt5_DIR/plugins/* $QT_PLUGINS_DIR/
+
+# add QT plugins dependencies
+find $Qt5_DIR/plugins/ | grep -e "\.so" | sed -e 's/\(.*\)/ldd \1/' | bash | grep -o '=> [^ ]*' | grep -o '[^=> ]*' | sed -e 's/^/cp /' -e 's@$@ '"$USR_LIB_DIR/"'@' | sort | uniq | bash -o xtrace
---
+++ scripts/deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# exit script on any error
+set -e
+set -o xtrace
+
+PROJECT_DIR=$(pwd)
+
+# Parse compiler versions from build directories
+cd bin
+GCC_VERSION=$(ls | grep -Po 'gcc-\d.\d.\d' | grep -Po '\d.\d.\d')
+CLANG_VERSION=$(ls | grep -Po 'clang-\d.\d.\d' | grep -Po '\d.\d.\d')
+
+# create AppImages
+cd $PROJECT_DIR
+
+cp -r appimage/* bin/gcc-$GCC_VERSION/debug/AppDir/
+AppRun bin/gcc-$GCC_VERSION/debug/AppDir
+mv BitmapViewer-x86_64.AppImage BitmapViewer-gcc-$GCC_VERSION-debug-x86_64.AppImage
+
+cp -r appimage/* bin/gcc-$GCC_VERSION/release/AppDir
+AppRun bin/gcc-$GCC_VERSION/release/AppDir
+mv BitmapViewer-x86_64.AppImage BitmapViewer-gcc-$GCC_VERSION-release-x86_64.AppImage
+
+cp -r appimage/* bin/clang-$CLANG_VERSION/debug/AppDir
+AppRun bin/clang-$CLANG_VERSION/debug/AppDir
+mv BitmapViewer-x86_64.AppImage BitmapViewer-clang-$CLANG_VERSION-debug-x86_64.AppImage
+
+cp -r appimage/* bin/clang-$CLANG_VERSION/release/AppDir
+AppRun bin/clang-$CLANG_VERSION/release/AppDir
+mv BitmapViewer-x86_64.AppImage BitmapViewer-clang-$CLANG_VERSION-release-x86_64.AppImage
+
+
+# upload created files to Platin server
+smbclient //$PLATIN_ADDRESS/Austausch -U "$PLATIN_USER%$PLATIN_PASS" -D //GitLab-Bot -c "mkdir BitmapViewer"
+smbclient //$PLATIN_ADDRESS/Austausch -U "$PLATIN_USER%$PLATIN_PASS" -D //GitLab-Bot/BitmapViewer -c "put BitmapViewer-gcc-$GCC_VERSION-debug-x86_64.AppImage"
+smbclient //$PLATIN_ADDRESS/Austausch -U "$PLATIN_USER%$PLATIN_PASS" -D //GitLab-Bot/BitmapViewer -c "put BitmapViewer-gcc-$GCC_VERSION-release-x86_64.AppImage"
+smbclient //$PLATIN_ADDRESS/Austausch -U "$PLATIN_USER%$PLATIN_PASS" -D //GitLab-Bot/BitmapViewer -c "put BitmapViewer-clang-$CLANG_VERSION-debug-x86_64.AppImage"
+smbclient //$PLATIN_ADDRESS/Austausch -U "$PLATIN_USER%$PLATIN_PASS" -D //GitLab-Bot/BitmapViewer -c "put BitmapViewer-clang-$CLANG_VERSION-release-x86_64.AppImage"
+
+# Remove created files
+rm BitmapViewer-*.AppImage
