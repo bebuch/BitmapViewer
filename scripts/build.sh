f8742491194c7fafa279fdc089f1de9a91689c85
Benjamin Buch <benni.buch@gmail.com>
2018-11-01 13:45:04 +0100

    qt plugins not required

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,7 +18,6 @@ BUILD_DIR=$BIN_DIR/build
 EXE_DIR=$BIN_DIR/AppDir/bin
 USR_BIN_DIR=$BIN_DIR/AppDir/usr/bin
 USR_LIB_DIR=$BIN_DIR/AppDir/usr/lib
-QT_PLUGINS_DIR=$BIN_DIR/AppDir/usr/qt/plugins
 
 
 # make build and deploy dirs
@@ -26,7 +25,6 @@ mkdir -p $BUILD_DIR
 mkdir -p $EXE_DIR
 mkdir -p $USR_BIN_DIR
 mkdir -p $USR_LIB_DIR
-mkdir -p $QT_PLUGINS_DIR
 
 
 # build
@@ -47,12 +45,6 @@ cp /bin/grep $USR_BIN_DIR
 # add dependencies
 ldd $MAIN_EXE
 ldd $MAIN_EXE | grep -e "\.so" | grep -o '=> [^ ]*' | grep -o '[^=> ]*' | sort | sed -e 's/^/cp /' -e 's@$@ '"$USR_LIB_DIR/"'@' | bash
-
-# add QT plugins
-cp -r $Qt5_DIR/plugins/* $QT_PLUGINS_DIR/
-
-# add QT plugins dependencies
-find $Qt5_DIR/plugins/ | grep -e "\.so" | sed -e 's/\(.*\)/ldd \1/' | bash | grep -o '=> [^ ]*' | grep -o '[^=> ]*' | sed -e 's/^/cp /' -e 's@$@ '"$USR_LIB_DIR/"'@' | sort | uniq | bash -o xtrace
 
 
 # create AppImage
